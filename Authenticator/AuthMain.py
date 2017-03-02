3aa83fe3633842842fbb0c4b4b68ae5653e0267a
xabier <xabier.jauregi@alumni.mondragon.edu>
2017-03-02 18:28:34 +0100

    Auth change

--- Authenticator/AuthMain.py
+++ Authenticator/AuthMain.py
@@ -5,21 +5,25 @@ import ChapCodes
 
 if __name__ == "__main__":
     try:
+        print("***************************************************")
+        print("               Server running....")
+        print("***************************************************")
 
         config = MyAuthFunctions.get_config_values()
-        print "============ Starting authentication process as authenticator ================"
 
         sock = MyAuthFunctions.listen(config)
         packet = MyPackeTManager.receive_packet(sock)
-        if (packet['code'] == ChapCodes.AUTH_REQUEST):
+
+        if packet['code'] == ChapCodes.AUTH_REQUEST:
             auth_request_data = MyAuthFunctions.process_authentication_request(packet)
-            (packet, challenge_identifier, challenge) = MyAuthFunctions.create_challenge(config, auth_request_data)
+            (packet, challenge_id, challenge) = MyAuthFunctions.create_challenge(config)
             MyPackeTManager.send_packet(sock, packet)
             packet = MyPackeTManager.receive_packet(sock)
-            if (packet['code'] == ChapCodes.RESPONSE):
-                if (packet['identifier'] == challenge_identifier):
+
+            if packet['code'] == ChapCodes.RESPONSE:
+                if packet['identifier'] == challenge_id:
                     response_data = MyAuthFunctions.process_response(packet)
-                    if (MyAuthFunctions.verify_response(response_data, auth_request_data['identity'], challenge_identifier, challenge)):
+                    if MyAuthFunctions.verify_response(response_data, auth_request_data['identity'], challenge_id, challenge):
                         code = ChapCodes.SUCCESS
                         data = ''
                     else:
@@ -31,5 +35,5 @@ if __name__ == "__main__":
         sock.close()
 
     except EOFError:
-        print "\nCannot continue: End of File detected. Exiting..."
+        print ("\nCannot continue: End of File detected. Exiting...")
         sys.exit()
--- Authenticator/MyAuthFunctions.py
+++ Authenticator/MyAuthFunctions.py
@@ -18,76 +18,81 @@ def get_config_values():
             raise Exception('Configuration is not filled!')
 
     except Exception as e:
-        print "\nERROR: ", e
+        print("\nERROR: ", e)
         sys.exit()
 
     return config
 
+
 def check_config_values_are_filled(config):
     for c in config:
-        if (config[c] == ''):
+        if config[c] == '':
             return False
     return True
+
 
 def listen(config):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('', int(config['port'])))
-    print "Waiting for incoming authentication requests..."
-    sock.listen(5) # 5 = zenbat entzule ipintzen dian kolan
+    print("Waiting for incoming authentication requests...")
+    sock.listen(5)  # 5 = zenbat entzule ipintzen dian kolan
     (conn, addr) = sock.accept()
     return conn
+
 
 def verify_response(response_data, identity, identifier, challenge):
 
     secret = check_identity_in_database(identity)
-    if not(secret ==''):
+    if not secret == '':
         hash = hashlib.sha256(chr(identifier) + secret + challenge)
         our_value = hash.digest()
-        if (our_value == response_data['response']):
+        if our_value == response_data['response']:
             return 1
         else:
             return 0
     else:
         return 0
 
-def process_authentication_request(auth_request_packet):
-    identity = auth_request_packet['data']
-    print "Processing authentication request for identity:", identity
-    return {'identifier': auth_request_packet['identifier'],
-            'identity': identity}
 
-def create_challenge(config, auth_request_data):
+def process_authentication_request(packet):
+    identity = packet['data']
+    return {'identity': identity, 'identifier': packet['identifier']}
+
+
+def create_challenge(config):
 
     identifier = random.randint(0, 255)
 
-    # Hash-a unikua izanbiada orduan string aleatorio bat sotzen dot
+    # Hash-a bakarra izanbiada orduan string aleatorio bat sotzen dot
     hash = hashlib.sha256(generate_random_string())
 
     challenge_value = hash.digest()
     challenge_value_size = struct.pack('!B', len(challenge_value))
     name = config['localname']
     data = challenge_value_size + challenge_value + name
-    print "Creating challenge with identifier:", identifier
+    print ("Creating challenge with identifier:", identifier)
     packet = MyPackeTManager.createPacket(ChapCodes.CHALLENGE, identifier, data)
-    return (packet, identifier, challenge_value)
+    return packet, identifier, challenge_value
+
 
 def process_response(response_packet):
     response_len = struct.unpack('!B', response_packet['data'][0])[0]
     response = response_packet['data'][1:response_len + 1]
     name = response_packet['data'][response_len + 1:]
-    print "Processing response with identifier:", response_packet['identifier'], "name:", name
-    return {'identifier': response_packet['identifier'],
-            'response': response,
-            'name': name}
+    print("Processing response with identifier:", response_packet['identifier'], "name:", name)
+
+    return {'identifier': response_packet['identifier'], 'response': response, 'name': name}
+
 
 def generate_random_string():
     return str(uuid.uuid4())
 
+
 def check_identity_in_database(identity):
     identities = {}
-    identities['jaure'] = '123';
+    identities['jaure'] = '123'
 
-    if (identity in identities):
+    if identity in identities:
         return identities[identity]
     else:
-        ''
+        return ''
